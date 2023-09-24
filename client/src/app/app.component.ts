cc58e4c9873666681dcce5014b81cbd5596aa203
c285djb <manleyc1@gmatc.matc.edu>
2023-09-24 11:51:12 -0500

    Completed Activity 4

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client';
+  weatherForecasts: any;
+
+  constructor(private http: HttpClient){
+    
+  }
+  ngOnInit(): void{
+    this.http.get('http://localhost:5023/weatherforecast').subscribe(
+      response => { this.weatherForecasts = response; },
+      error => { console.log (error)}
+    );
+  }
 }
