54a55b6107db5a381eba81bd38d1151977610bcb
c285djb <manleyc1@gmatc.matc.edu>
2023-11-04 14:08:19 -0500

    Activity 9 Fourth Commit

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,15 +5,17 @@ import {MatCardModule} from '@angular/material/card';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
-
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
 import { ViewPostComponent } from './view-post/view-post.component';
 import { CreatePostComponent } from './create-post/create-post.component';
-import { AppRoutingModule } from './app-routing.module';
-import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
@@ -24,14 +26,19 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
     MatCardModule,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    AppRoutingModule
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
+  
+  
   providers: [],
   bootstrap: [AppComponent]
 })
